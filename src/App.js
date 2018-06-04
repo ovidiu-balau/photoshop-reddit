dbde62bda3366e594c8c404836d1a1dcce80afc3
Josie Porter <Ovidiu@Bens-Mac.local>
2018-06-04 14:48:55 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pictures: [],
+    }
+  }
+  componentDidMount() {
+    fetch('https://www.reddit.com/r/all.json')
+      .then(results => {
+        return results.json();
+      }).then(posts => {
+        const postList = posts.data.children;
+        let data = postList.map((post) => {
+          console.log(post.data)
+          if (post.data.over_18 == false) {
+            const tn = post.data.preview.images[0].source.url ? post.data.preview.images[0].source.url : post.data.thumbnail;
+            return (
+              <div className="post" key={post.data.id}>
+                <a href={post.data.url} target="_blank">
+                  <img className="thumbnail" src={post.data.preview.images[0].source.url} alt={post.data.title} />
+                </a>
+              </div>
+            )
+          } else {
+            return null;
+          }
+        });
+        this.setState({ pictures: data })
+      });
+    ;
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Photoshop reddit</h1>
+        </header>
+        <p className="App-intro">
+          {this.state.pictures}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
